719751820ffa9e6497a70d03e0cf26184c8b6d73
Chizh Vladimir <vovatakeda@mail.ru>
2024-01-14 23:10:45 +0300

    add some logs

--- pkg/handlers/http/info.go
+++ pkg/handlers/http/info.go
@@ -3,10 +3,12 @@ package http
 import (
 	"ChatServer/pkg/types"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func (h *Handler) GetUsersByPrefix(c *gin.Context) {
+	logrus.Infoln("Got search user request")
 
 	var getUsersByPrefixReq types.GetUsersByPrefixReq
 
@@ -15,11 +17,15 @@ func (h *Handler) GetUsersByPrefix(c *gin.Context) {
 		return
 	}
 
+	logrus.Infoln("With prefix: ", getUsersByPrefixReq.Prefix)
+
 	users, err := h.services.GetUsersByPrefix(c, getUsersByPrefixReq.Prefix)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{})
 		return
 	}
+
+	logrus.Infoln("Answer: ", users)
 
 	c.JSON(http.StatusOK, users)
 }
